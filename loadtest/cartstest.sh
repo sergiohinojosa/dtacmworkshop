71a66685ac7f96052b5011f969b015b201d82448
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2019-10-21 13:17:50 +0200

    masking /

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,6 +1,6 @@
 # Using ingress for accessing all services in the microk8s environment 
 # See ingress folder for the definition of the services.
 export K8_IP=$(curl ifconfig.me)
-export CARTS_URL=http://carts.dev.$K8_IP.xip.io
-sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_URL/g" carts_load1.jmx
-sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_URL/g" carts_load2.jmx
+export CARTS_DNS=http:\/\/carts.dev.$K8_IP.xip.io
+sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_DNS/g" carts_load1.jmx
+sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_DNS/g" carts_load2.jmx
