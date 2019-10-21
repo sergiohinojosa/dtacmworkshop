cb0b23ebdfc21aa2b65b73f15c912aeae08dd1f0
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2019-10-21 13:29:14 +0200

    replacing curl with wget

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,6 +1,7 @@
 # Using ingress for accessing all services in the microk8s environment 
 # See ingress folder for the definition of the services.
-export K8_IP=$(curl ifconfig.me)
+#  using wget -qO- http://ifconfig.me (quitet mode and pipeout) instead of curl since curl is not in the container.
+export K8_IP=$(wget -qO- http://ifconfig.me)
 export CARTS_DNS=http:\/\/carts.dev.$K8_IP.xip.io
 sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_DNS/g" carts_load1.jmx
 sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_DNS/g" carts_load2.jmx
