ad26032a090ac03ceba1cbccda96e2915e488800
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2019-10-21 15:21:01 +0200

    replacing / for %

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -2,6 +2,6 @@
 # See ingress folder for the definition of the services.
 #  using wget -qO- http://ifconfig.me (quitet mode and pipeout) instead of curl since curl is not in the container.
 export K8_IP=$(wget -qO- http://ifconfig.me)
-export CARTS_DNS=http://carts.dev.$K8_IP.xip.io
+export CARTS_DNS=carts.dev.$K8_IP.xip.io
 sed -i "s%CARTS_URL_PLACEHOLDER%$CARTS_DNS%g" carts_load1.jmx
 sed -i "s%CARTS_URL_PLACEHOLDER%$CARTS_DNS%g" carts_load2.jmx
